05afbdfb8260dfd60530e1b66941f624bda75c2b
Juan Patricio <juanpibot@gmail.com>
2019-09-25 13:26:13 -0300

    me olvide de comitear la config de hibernate

--- RoyalAcademy/src/dao/HibernateUtil.java
+++ RoyalAcademy/src/dao/HibernateUtil.java
@@ -21,7 +21,7 @@ public class HibernateUtil {
                 settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
                 settings.put(Environment.URL, "jdbc:mysql://localhost:3306/royal_academy?allowPublicKeyRetrieval=true&useSSL=false");
                 settings.put(Environment.USER, "root");
-                settings.put(Environment.PASS, "");
+                settings.put(Environment.PASS, "root");
                 settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
                 settings.put(Environment.SHOW_SQL, "true");
                 settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
@@ -46,6 +46,8 @@ public class HibernateUtil {
                 configuration.addAnnotatedClass(Sede_Carrera.class);
                 configuration.addAnnotatedClass(PermisoFunciones.class);
                 //configuration.addAnnotatedClass(AlumnoNotas.class);
+                configuration.addAnnotatedClass(Sede_Usuario.class);
+                configuration.addAnnotatedClass(Curso_Usuario.class);  
                 
                 // GUARDO CONFIGURACION
                 ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
